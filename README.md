2d4afb2e783821e295ccb5f09dfeebbc6fafacab
AnnaHuynhLy <annahuynhly012920@gmail.com>
2026-03-24 20:12:07 -0400

    trying to add R CMD check workflow

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# nicheR.experiment
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of nicheR.experiment is to ...
+
+## Installation
+
+You can install the development version of nicheR.experiment from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("pak")
+pak::pak("annahuynhly/nicheR_experiment")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(nicheR.experiment)
+## basic example code
+```
+
--- misc/experiment.R
+++ misc/experiment.R
@@ -1,9 +1,13 @@
 
+# For the "easy" GSOC test.
+
+## loading the packages
 library("terra")
 library("geodata")
 
 ??geodata::worldclim_global
 
+## downloading the correct data
 dat <- geodata::worldclim_global(var = "bio", res = 10)
 
 saveRDS(dat, "data/worldclim_bio.rds")
@@ -14,31 +18,21 @@ dat <- terra::unwrap(dat)
 
 p <- plot(dat[[1]])
 
-# masked version
-#lat <- c(-56.557, -55.795,-51.779,
-#         -24.092, -6.357, 10.857,   12.794, -1.177, -19.114, -49.167,
-#         -54.726)
-#lon <- c(-69.852, -61.442,  -56.279,
-#         -40.239, -32.577,  -60.488,  -75.52, -83.498, -72.822, -77.514,
-#         -75.403)
-#x <- cbind(lon, lat)
-
-# remove
-lon <- c(-82, -34, -34, -82, -82)
-lat <- c(-56, -56,  13,  13, -56)
-x <- cbind(lon, lat)
-polygon(x, col=rgb(1, 0, 0,0.5), border='red')
-
-############# 
+############# cropping
 sa_ext <- ext(-82, -34, -56, 13)
 bio1_sa <- crop(dat[[1]], sa_ext)
 terra::plot(bio1_sa)
 
-#############
+############# masking
 sa_ext <- ext(-82, -34, -56, 13)
 bio1_sa <- mask(dat[[1]], sa_ext)
 terra::plot(bio1_sa)
 
 
+# extra; just covering with a red box, experimental.
+lon <- c(-82, -34, -34, -82, -82)
+lat <- c(-56, -56,  13,  13, -56)
+x <- cbind(lon, lat)
+polygon(x, col=rgb(1, 0, 0,0.5), border='red')
 
 
